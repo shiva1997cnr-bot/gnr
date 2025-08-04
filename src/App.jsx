4a4e1273f99b53f8ddaf647f280270f418660194
Shiv <shiv@example.com>
2025-08-04 11:19:30 +0530

    Fix: Responsive layout for all devices

--- src/App.jsx
+++ src/App.jsx
@@ -32,11 +32,27 @@ const router = createBrowserRouter([
   { path: "/userid", element: <UserId /> },
   { path: "/intro", element: <Intro /> },
   { path: "/Register", element: <Register /> },
-  { path: "/scores", element: <Scores/> },
+  { path: "/scores", element: <Scores /> },
 ]);
 
 function App() {
   return <RouterProvider router={router} />;
 }
+<h1
+  className="text-5xl font-extrabold bg-gradient-to-r from-purple-300 via-pink-400 to-red-400 bg-clip-text text-transparent animate-shimmer tracking-wider drop-shadow-md mb-2"
+>
+  Generalist
+</h1>
 
 export default App;
+{
+<div className="container mx-auto px-4">
+  {/* content here */}
+</div>
+}
+
+{<div className="w-full max-w-screen-xl px-4 sm:px-6 lg:px-8 mx-auto">
+  {/* your main content here */}
+</div>
+}
+
