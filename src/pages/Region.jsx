d3aad3032d997bc8b10f195d48b4b5fd786c0455
Shiv <shiv@example.com>
2025-08-05 20:28:00 +0530

    fix: correct path to app.css in Region.jsx

--- src/pages/Region.jsx
+++ src/pages/Region.jsx
@@ -1,7 +1,7 @@
 import React, { useRef, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import hoverSound from "../assets/hover.mp3";
-import "../../app.css";
+import "../app.css";
 
 
 const Region = () => {
