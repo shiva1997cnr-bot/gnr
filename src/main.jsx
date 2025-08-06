e30eea7b1bd083ae7ce987a970213ed0c69c3c4b
Shiv <shiv@example.com>
2025-08-06 08:57:46 +0530

    Initial commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
-import './app.css'; // ✅ Import here if styles are used across the app
+import './styles/app.css'; // ✅ Import here if styles are used across the app
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
